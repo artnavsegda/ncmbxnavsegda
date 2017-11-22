2830b44c5eb3420305d59d59d2c57c8e60c06938
Art Navsegda <artnavsegda@gmail.com>
2017-11-22 05:34:27 +0300

    RTU

--- src/ncmodbusrtu.c
+++ src/ncmodbusrtu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <ncurses.h>
 #include <modbus/modbus.h>
 
@@ -8,6 +9,15 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("name serial port\n");
+		exit(1);
+	}
+	modbus_t *mb = modbus_new_rtu(argv[1], 9600, 'N', 8, 1);
+	modbus_set_slave(mb, 50);
+	modbus_rtu_set_serial_mode(mb, MODBUS_RTU_RS232);
+	if (modbus_connect(mb) == -1)
+	{
+		printf("Connection failed: %s\n", modbus_strerror(errno));
+		modbus_free(mb);
 		exit(1);
 	}
 	return 0;
