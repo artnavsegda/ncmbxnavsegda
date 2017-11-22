8bef52b33adbb58beeb859920e1ed175f8314de0
Art Navsegda <artnavsegda@gmail.com>
2017-11-22 04:10:33 +0300

    startup

---
+++ run-clean.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+make clean
+make distclean
+rm -rvf aclocal.m4 autom4te.cache compile config.h config.h.in config.log config.status configure depcomp install-sh Makefile Makefile.in missing stamp-h1
+rm src/Makefile.in
